1f7d348202b7bf6356f3d38ef96e8362f6b407c2
jthomaz <jthomaz@brasilcap.com.br>
2019-03-29 16:01:43 -0300

    iníciando processo de login

--- app/src/main/java/br/com/njinformatica/easyvote/MainActivity.kt
+++ app/src/main/java/br/com/njinformatica/easyvote/MainActivity.kt
@@ -2,11 +2,38 @@ package br.com.njinformatica.easyvote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import br.com.njinformatica.easyvote.viewmodel.LoginViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginViewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+
+        subscribe()
+        setupListeners()
     }
+
+    private fun subscribe(){
+        loginViewModel.isLogged.observe(this,  Observer {
+            if (it) {
+                Toast.makeText(this, "Usuário logado", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun setupListeners(){
+        btn_ok.setOnClickListener {
+            loginViewModel.login();
+        }
+    }
+
 }
--- app/src/main/java/br/com/njinformatica/easyvote/viewmodel/LoginViewModel.kt
+++ app/src/main/java/br/com/njinformatica/easyvote/viewmodel/LoginViewModel.kt
@@ -1,45 +1,22 @@
 package br.com.njinformatica.easyvote.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
 /**
  * Created by jthomaz on 29/03/2019.
  */
-class LoginViewModel {
-
-    private var _errorMessage: String = ""
-        set(value) {
-            field = value
-            errorMessage.value = value
-        }
+class LoginViewModel : ViewModel() {
 
     val errorMessage: MutableLiveData<String> = MutableLiveData()
     val isLoading = MutableLiveData<Boolean>()
+    var isLogged = MutableLiveData<Boolean>()
+
+    init{
+        isLogged.value = false;
+    }
 
     fun login() {
-        /*
-        val call = RetrofitProvider.newsAPI.getAllNews()
-        isLoading.value = true
-        call.enqueue(object : Callback<NewsResponseObject> {
-            override fun onFailure(call: Call<NewsResponseObject>, t: Throwable) {
-                _errorMessage = "Deu ruim: ${t.message}"
-                isLoading.value = false
-            }
-
-            override fun onResponse(call: Call<NewsResponseObject>,
-                                    response: Response<NewsResponseObject>
-            ) {
-                if (response.isSuccessful) {
-                    response.body()?.let { newsResponseObject ->
-                        newsList.value = newsResponseObject.news
-                    }
-                } else {
-                    _errorMessage = "Deu ruim, fale com o admin"
-                }
-                isLoading.value = false
-            }
-
-        })
-        */
+        isLogged.value = true;
     }
 }
